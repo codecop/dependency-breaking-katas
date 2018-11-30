dca845c3ced730a2b58dcf89ce528ea73eec4d46
Roland.Germ <roland.germ@anecon.com>
2018-11-30 11:51:45 +0100

    Test green (but only today).

--- Java/src/test/java/org/codecop/dependencies/subclass_and_override/MarketingCampaignTest.java
+++ Java/src/test/java/org/codecop/dependencies/subclass_and_override/MarketingCampaignTest.java
@@ -12,6 +12,34 @@ public class MarketingCampaignTest {
 
         boolean isCrazySalesDay = campaign.isCrazySalesDay();
 
-        assertFalse(isCrazySalesDay);
+        assertTrue(isCrazySalesDay);
     }
 }
+
+/*
+
+1. run tests -> red or green
+2. falls red -> was ist das Problem
+3. ich sehe es nicht weil kein gescheites Assert
+   3.1. change assert
+4. test anpassen?
+   green
+   run with coverage -> 1 von 2 branches rot.
+5. add more test cases
+   egal was wir machen, es ist immer today (Freitag)
+   5.1. override dayOfWeek
+        (viel weniger invasiv)
+   5.2. Timer interface auslagern mit 2 Methoden
+        (mehr Arbeit - mehr Risiko, dafür SOLID)
+        TODO wie kann man den Code so machen, dass er rausziehen will
+        - Versprechen von Reuse
+        - Zwang von Austauschbarkeit (OCP) -> Strategy
+        - sichtbar nicht cohesive (SRP)
+        - ist kompliziert und passt nicht zum Rest
+        - Kopplung/LoD -> Middle Man
+   5.3. power mock (stub LocalDate)
+
+6. modify code via 5.1, only automatic?, minimal code change in production?
+
+7. add more test cases
+ */
