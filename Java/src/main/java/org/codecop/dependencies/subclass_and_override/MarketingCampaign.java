b46865a4bef6fc82af78356e41217c29721a128a
Roland.Germ <roland.germ@anecon.com>
2018-11-30 12:30:35 +0100

    Add second seam for member access. Two variants now: direct static call, field access.

--- Java/src/main/java/org/codecop/dependencies/subclass_and_override/MarketingCampaign.java
+++ Java/src/main/java/org/codecop/dependencies/subclass_and_override/MarketingCampaign.java
@@ -2,11 +2,22 @@ package org.codecop.dependencies.subclass_and_override;
 
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.util.function.LongSupplier;
 
 public class MarketingCampaign {
 
+    private final LongSupplier systemTimeMillis;
+
+    public MarketingCampaign() {
+        systemTimeMillis = System::currentTimeMillis;
+    }
+
     public boolean isActive() {
-        return System.currentTimeMillis() % 2 == 0;
+        return milliSeconds() % 2 == 0;
+    }
+
+    private long milliSeconds() {
+        return systemTimeMillis.getAsLong();
     }
 
     public boolean isCrazySalesDay() {
