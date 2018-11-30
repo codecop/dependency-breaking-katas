7639eec514614137beffcff87217230356edf613
Roland.Germ <roland.germ@anecon.com>
2018-11-30 09:34:49 +0100

    make existing test pass

--- Java/src/test/java/org/codecop/dependencies/parameterise_constructor/DiscountTest.java
+++ Java/src/test/java/org/codecop/dependencies/parameterise_constructor/DiscountTest.java
@@ -16,7 +16,24 @@ public class DiscountTest {
         final Money net = new Money(1002);
         final Money total = discount.discountFor(net);
 
-        Assert.assertEquals(new Money(new BigDecimal("901.8")), total);
+        Assert.assertEquals(new Money(new BigDecimal("851.7")), total);
     }
 
+
 }
+
+/*
+
+1. run tests -> red or green
+2. falls red -> was ist das Problem
+3. ich sehe es nicht weil Money kein toString hat
+   3.1. debuggen
+        breakpoint, starten, 2x auffalten
+        test anpassen? -> 851.7
+        green
+        run with coverage
+   3.2. add toString
+        darf ich das mit einer Methode die polymorph aufgerufen wird. eigentlich nicht.
+   3.3. matcher für Money
+
+ */
