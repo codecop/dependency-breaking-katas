d46688cfef78af40ca0f939b035bb7fff849b2ee
Peter Kofler <peter.kofler@code-cop.org>
2018-12-08 22:35:16 +0100

    Adds C# project structure with sample test case.

---
+++ CSharp/DependencyBreakingKatas/Hello.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Org.Codecop.Dependencies
+{
+    public class Hello
+    {
+        public virtual string greetings()
+        {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ CSharp/DependencyBreakingTests/HelloTests.cs
@@ -0,0 +1,18 @@
+﻿using Org.Codecop.Dependencies;
+using System;
+using Xunit;
+
+namespace Org.Codecop.Dependencies.Tests
+{
+    public class HelloTest
+    {
+        [Fact]
+        public void ShouldGreet()
+        {
+            var hello = new Hello();
+
+            var greeting = hello.greetings();
+
+            Assert.Equal("Hello World", greeting);
+        }
+}
