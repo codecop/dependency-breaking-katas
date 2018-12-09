f1c37ba30228e82a319b70f99252f9cfa1d31464
Roland.Germ <roland.germ@anecon.com>
2018-12-09 14:59:46 +0100

    Implemented basic JDBC insert

--- Java/src/main/java/org/codecop/dependencies/extract_and_overide_call/Receipt.java
+++ Java/src/main/java/org/codecop/dependencies/extract_and_overide_call/Receipt.java
@@ -4,17 +4,28 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Receipt {
-
     private Money amount;
     private Money tax;
     private Money total;
+
+    public Money getAmount() {
+        return amount;
+    }
 
     public void setAmount(Money amount) {
         this.amount = amount;
     }
 
+    public Money getTax() {
+        return tax;
+    }
+
     public void setTax(Money tax) {
         this.tax = tax;
+    }
+
+    public Money getTotal() {
+        return total;
     }
 
     public void setTotal(Money total) {
@@ -31,5 +42,4 @@ public class Receipt {
                 "Total  ... " + total.format() //
         );
     }
-
 }
--- Java/src/main/java/org/codecop/dependencies/extract_and_overide_call/ReceiptRepository.java
+++ Java/src/main/java/org/codecop/dependencies/extract_and_overide_call/ReceiptRepository.java
@@ -1,11 +1,40 @@
 package org.codecop.dependencies.extract_and_overide_call;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ReceiptRepository {
+    public static final String URL = "jdbc:mysql://localhost:3306/myShop";
+    public static final String USER = "root";
+    public static final String PASS = "root";
 
     public static void store(Receipt receipt) {
-        // access database and do evil side effects ;-)
-        System.exit(1);
-        // TODO implement basic JDBC logic
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.err.println(ex.getMessage());
+            throw new RuntimeException("Could not load DB driver", ex);
+        }
+
+        String sqlInsert = "insert into RECEIPT "
+                + "(AMOUNT, TAX, TOTAL)"
+                + "values(?, ?, ?)";
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASS);
+             PreparedStatement statement = connection.prepareStatement(sqlInsert);
+        ) {
+            statement.setBigDecimal(1, receipt.getAmount().asBigDecimal());
+            statement.setBigDecimal(2, receipt.getTax().asBigDecimal());
+            statement.setBigDecimal(3, receipt.getTotal().asBigDecimal());
+            statement.executeUpdate();
+
+        } catch (SQLException ex) {
+            System.err.println(ex.getMessage());
+            throw new RuntimeException("Database error", ex);
+        }
+
     }
 
 }
