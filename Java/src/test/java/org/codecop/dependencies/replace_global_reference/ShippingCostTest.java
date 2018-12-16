2c3de59395839aa797fdb0a321c36bffe32d5bb2
Peter Kofler <peter.kofler@code-cop.org>
2018-12-16 13:39:18 +0100

    Assert that the given country is the expected one.

--- Java/src/test/java/org/codecop/dependencies/replace_global_reference/ShippingCostTest.java
+++ Java/src/test/java/org/codecop/dependencies/replace_global_reference/ShippingCostTest.java
@@ -8,81 +8,91 @@ public class ShippingCostTest {
 
     @Test
     public void europeShipsAtCostOf5() {
-        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInEurope();
+        Country germany = new Country("DE");
+        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInEurope(germany);
         ShippingCost shippingCost = createShippingCostForTest(googleMapsAPIStub);
 
-        Money cost = shippingCost.calculate(new Country("DE"), DeliveryOptions.STANDARD);
+        Money cost = shippingCost.calculate(germany, DeliveryOptions.STANDARD);
 
         assertEquals(new Money(5), cost);
     }
 
     @Test
     public void americasShipsAtCostOf15() {
-        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInAmericas();
+        Country canada = new Country("CA");
+        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInAmericas(canada);
         ShippingCost shippingCost = createShippingCostForTest(googleMapsAPIStub);
 
-        Money cost = shippingCost.calculate(new Country("CA"), DeliveryOptions.STANDARD);
+        Money cost = shippingCost.calculate(canada, DeliveryOptions.STANDARD);
 
         assertEquals(new Money(15), cost);
     }
 
     @Test
     public void americasExpressShipsAtCostOf25() {
-        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInAmericas();
+        Country canada = new Country("CA");
+        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInAmericas(canada);
         ShippingCost shippingCost = createShippingCostForTest(googleMapsAPIStub);
 
-        Money cost = shippingCost.calculate(new Country("CA"), DeliveryOptions.EXPRESS);
+        Money cost = shippingCost.calculate(canada, DeliveryOptions.EXPRESS);
 
         assertEquals(new Money(40), cost);
     }
 
     @Test
     public void otherShipsAtCostWithDistance() {
-        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInOtherWithDistance(2380);
+        Country egypt = new Country("EG");
+        GoogleMapsAPI googleMapsAPIStub = createGoogleMapAPIInOtherWithDistance(egypt, 2380);
         ShippingCost shippingCost = createShippingCostForTest(googleMapsAPIStub);
 
-        Money cost = shippingCost.calculate(new Country("EG"), DeliveryOptions.EXPRESS);
+        Money cost = shippingCost.calculate(egypt, DeliveryOptions.EXPRESS);
 
         assertEquals(new Money(238), cost);
     }
 
-    private GoogleMapsAPI createGoogleMapAPIInEurope() {
+    private GoogleMapsAPI createGoogleMapAPIInEurope(Country expectedCountry) {
         return new GoogleMapsAPI() {
             @Override
             public boolean isInCommonMarket(Country country) {
+                assertEquals(expectedCountry, country);
                 return true;
             }
         };
     }
 
-    private GoogleMapsAPI createGoogleMapAPIInAmericas() {
+    private GoogleMapsAPI createGoogleMapAPIInAmericas(Country expectedCountry) {
         return new GoogleMapsAPI() {
             @Override
             public boolean isInCommonMarket(Country country) {
+                assertEquals(expectedCountry, country);
                 return false;
             }
 
             @Override
             public boolean isInAmericas(Country country) {
+                assertEquals(expectedCountry, country);
                 return true;
             }
         };
     }
 
-    private GoogleMapsAPI createGoogleMapAPIInOtherWithDistance(int distance) {
+    private GoogleMapsAPI createGoogleMapAPIInOtherWithDistance(Country expectedCountry, int distance) {
         return new GoogleMapsAPI() {
             @Override
             public boolean isInCommonMarket(Country country) {
+                assertEquals(expectedCountry, country);
                 return false;
             }
 
             @Override
             public boolean isInAmericas(Country country) {
+                assertEquals(expectedCountry, country);
                 return false;
             }
 
             @Override
             public int distanceTo(Country country) {
+                assertEquals(expectedCountry, country);
                 return distance;
             }
         };
