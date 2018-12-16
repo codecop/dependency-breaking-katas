2799676f1532802edc00e6f492e857b58ee628e6
Roland.Germ <roland.germ@anecon.com>
2018-12-16 15:21:18 +0100

    Implemented real REST call and added description for assignment.

--- Java/src/main/java/org/codecop/dependencies/extract_and_override_call/SQLRuntimeException.java
+++ Java/src/main/java/org/codecop/dependencies/extract_and_override_call/SQLRuntimeException.java
@@ -2,7 +2,7 @@ package org.codecop.dependencies.extract_and_override_call;
 
 public class SQLRuntimeException extends RuntimeException {
 
-    private static final long serialVersionUID = -7774153411692345736L;
+    private static final long serialVersionUID = -1L;
 
     public SQLRuntimeException(String message, Throwable cause) {
         super(message, cause);
--- Java/src/main/java/org/codecop/dependencies/replace_global_reference/RestCountriesAPIException.java
+++ Java/src/main/java/org/codecop/dependencies/replace_global_reference/RestCountriesAPIException.java
@@ -2,11 +2,14 @@ package org.codecop.dependencies.replace_global_reference;
 
 public class RestCountriesAPIException extends RuntimeException {
 
+    private static final long serialVersionUID = -7774153411692345736L;
+
     public RestCountriesAPIException(String message) {
         super(message);
     }
-    public RestCountriesAPIException(String message, Throwable e) {
-        super(message, e);
+
+    public RestCountriesAPIException(String message, Throwable cause) {
+        super(message, cause);
     }
 
 }
