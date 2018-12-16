7716e2308488cb981dad1533f1c7c830b7c8dce0
Peter Kofler <peter.kofler@code-cop.org>
2018-12-16 13:14:45 +0100

    Add serial version UID (fix warning).

--- Java/src/main/java/org/codecop/dependencies/extract_and_override_factory/OrderCancelledException.java
+++ Java/src/main/java/org/codecop/dependencies/extract_and_override_factory/OrderCancelledException.java
@@ -2,6 +2,8 @@ package org.codecop.dependencies.extract_and_override_factory;
 
 public class OrderCancelledException extends RuntimeException {
 
+    private static final long serialVersionUID = 9042143130337520025L;
+
     public OrderCancelledException(Product product) {
         super(product.name());
     }
