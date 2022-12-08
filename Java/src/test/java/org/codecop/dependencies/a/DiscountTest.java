bcb4e8efbd07fe6e2a13c9ef64c79d754e27a74f
Tony Trung Thanh Vo <ttrung.vo@gmail.com>
2022-12-08 00:35:06 +0000

    Cleanup Tests

--- Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
+++ Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
@@ -11,25 +11,24 @@ import static org.mockito.Mockito.when;
 
 public class DiscountTest {
 
-    @Test
-    public void test1() {
-        Discount discount = new Discount(new MarketingCampaign());
 
+
+    @Test
+    public void reduce10IfNetPriceOver1000() {
+        Discount discount = new Discount(new MarketingCampaign());
         Money net = new Money(1002);
+
         Money total = discount.discountFor(net);
 
         assertEquals(new Money(new BigDecimal("901.8")), total);
     }
 
     @Test
-    public void ifIsCrazySalesDayReturnInputPrice() {
-
-
+    public void discount15ForCrazySaleDay() {
         MarketingCampaign marketingCampaign = mock(MarketingCampaign.class);
         when(marketingCampaign.isCrazySalesDay()).thenReturn(true);
 
         Discount discount = new Discount(marketingCampaign);
-
         Money money = discount.discountFor(Money.ONE_HUNDRED);
 
         assertEquals(new Money(new BigDecimal("85")), money);
