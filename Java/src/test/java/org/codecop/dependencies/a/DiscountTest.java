8e1b1a8d546d93d48ea5c2f1dd689912df40f1eb
Peter Kofler <peter.kofler@code-cop.org>
2024-02-13 13:45:26 +0100

    Parameterise constructor to control MarketingCampaign.

--- Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
+++ Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
@@ -8,13 +8,33 @@ import static org.junit.Assert.assertEquals;
 public class DiscountTest {
 
     @Test
-    public void test1() {
-        Discount discount = new Discount();
+    public void moreThanOneThousand() {
+        Discount discount = new Discount(new MarketingCampaign() {
+            @Override
+            public boolean isCrazySalesDay() {
+                return false;
+            }
+        });
 
         Money net = new Money(1002);
         Money total = discount.discountFor(net);
 
         assertEquals(new Money(new BigDecimal("901.8")), total);
+    }
+
+    @Test
+    public void crazySalesDay() {
+        Discount discount = new Discount(new MarketingCampaign() {
+            @Override
+            public boolean isCrazySalesDay() {
+                return true;
+            }
+        });
+
+        Money net = new Money(100);
+        Money total = discount.discountFor(net);
+
+        assertEquals(new Money(new BigDecimal("85")), total);
     }
 
 }
