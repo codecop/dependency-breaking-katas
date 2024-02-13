78c478231e939ffa4ed57a48de781d5fde08786f
Peter Kofler <peter.kofler@code-cop.org>
2024-02-13 13:53:16 +0100

    Mocked versions of a.Discount.

--- Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
+++ Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
@@ -1,9 +1,12 @@
 package org.codecop.dependencies.a;
 
-import java.math.BigDecimal;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+
+import org.junit.Test;
 
 public class DiscountTest {
 
@@ -30,6 +33,30 @@ public class DiscountTest {
                 return true;
             }
         });
+
+        Money net = new Money(100);
+        Money total = discount.discountFor(net);
+
+        assertEquals(new Money(new BigDecimal("85")), total);
+    }
+
+    @Test
+    public void moreThanOneThousandMocked() {
+        MarketingCampaign marketingCampaign = mock(MarketingCampaign.class);
+        // when(marketingCampaign.isCrazySalesDay()).thenReturn(true);
+        Discount discount = new Discount(marketingCampaign);
+
+        Money net = new Money(1002);
+        Money total = discount.discountFor(net);
+
+        assertEquals(new Money(new BigDecimal("901.8")), total);
+    }
+
+    @Test
+    public void crazySalesDayMocked() {
+        MarketingCampaign marketingCampaign = mock(MarketingCampaign.class);
+        when(marketingCampaign.isCrazySalesDay()).thenReturn(true);
+        Discount discount = new Discount(marketingCampaign);
 
         Money net = new Money(100);
         Money total = discount.discountFor(net);
