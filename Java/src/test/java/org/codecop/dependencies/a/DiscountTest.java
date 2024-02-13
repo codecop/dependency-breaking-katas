b57db0c073ba9294f1c510e55e2fbecdd9c45b9d
Peter Kofler <peter.kofler@code-cop.org>
2024-02-13 20:43:10 +0100

    solved a.

--- Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
+++ Java/src/test/java/org/codecop/dependencies/a/DiscountTest.java
@@ -4,17 +4,33 @@ import java.math.BigDecimal;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class DiscountTest {
 
     @Test
-    public void test1() {
-        Discount discount = new Discount();
+    public void discountForMoreThanThousand() {
+        MarketingCampaign marketingCampaign = mock(MarketingCampaign.class);
+        when(marketingCampaign.isCrazySalesDay()).thenReturn(false);
+        Discount discount = new Discount(marketingCampaign);
 
         Money net = new Money(1002);
         Money total = discount.discountFor(net);
 
         assertEquals(new Money(new BigDecimal("901.8")), total);
+    }
+
+    @Test
+    public void discountForCrazySalesDay() {
+        MarketingCampaign marketingCampaign = mock(MarketingCampaign.class);
+        when(marketingCampaign.isCrazySalesDay()).thenReturn(true);
+        Discount discount = new Discount(marketingCampaign);
+        
+        Money net = new Money(100);
+        Money total = discount.discountFor(net);
+        
+        assertEquals(new Money(85), total);
     }
 
 }
