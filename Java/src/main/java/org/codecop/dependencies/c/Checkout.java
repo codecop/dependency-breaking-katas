f64ee2fb321170312b02f9c316e3ced7e4f840e3
Peter Kofler <peter.kofler@code-cop.org>
2024-02-13 22:00:35 +0100

    Demonstrate Peel.

--- Java/src/main/java/org/codecop/dependencies/c/Checkout.java
+++ Java/src/main/java/org/codecop/dependencies/c/Checkout.java
@@ -3,15 +3,20 @@ package org.codecop.dependencies.c;
 public class Checkout {
 
     public Receipt createReceipt(Money amount) {
+        Receipt receipt = create(amount);
+
+        store(receipt);
+
+        return receipt;
+    }
+
+    protected Receipt create(Money amount) {
         Receipt receipt = new Receipt();
         Money vat = amount.percentage(20);
 
         receipt.setAmount(amount);
         receipt.setTax(vat);
         receipt.setTotal(amount.add(vat));
-
-        store(receipt);
-
         return receipt;
     }
 
